a09051541e5cabf20a440a352236c3f9877c9e67
Xinoji <jonathan.cn@live.com>
2023-06-01 13:33:38 -0600

    added testing enviroment

---
+++ Testing/Modelo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing
+{
+    #region clases
+    internal class Clase
+    {
+        int NRC { get; set; }
+        int clave { get; set; }
+        string nombre { get; set; }
+        List<Horario> horarios { get; set; }
+        string maestro { get; set; }
+
+        Clase() 
+        {
+            horarios = new List<Horario>();
+        }
+
+    }
+    internal class Horario 
+    { 
+        
+        
+    }
+    #endregion
+}
---
+++ Testing/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Xml;
+using HtmlAgilityPack;
+
+
+namespace Testing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"test.html";
+
+            var doc = new HtmlDocument();
+            doc.Load(path);
+            var tables = doc.DocumentNode.SelectNodes("//table[1]//tbody");
+            var table = tables.First();
+                Console.WriteLine("Found: " + table.Id);
+                foreach (HtmlNode row in table.SelectNodes("tr"))
+                {
+                    Console.WriteLine("row");
+                    foreach (HtmlNode cell in row.SelectNodes("th|td[@class='tddatos']"))
+                    {
+                        Console.WriteLine("cell: " + cell.InnerText);
+                    }
+                }
+            
+        }
+    }
+}
