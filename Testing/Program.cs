19d0c624e1a3bcc18c8fdb689abd932154174f0b
Xinoji <jonathan.cn@live.com>
2023-06-01 23:07:21 -0600

    testing with some XPaths

--- Testing/Program.cs
+++ Testing/Program.cs
@@ -15,15 +15,34 @@ namespace Testing
             doc.Load(path);
             var tables = doc.DocumentNode.SelectNodes("//table[1]//tbody");
             var table = tables.First();
-                Console.WriteLine("Found: " + table.Id);
-                foreach (HtmlNode row in table.SelectNodes("tr"))
+            Console.WriteLine("Found: " + table.XPath);
+            foreach (HtmlNode row in table.SelectNodes("tr[position() > 2]"))
+            {
+                Console.WriteLine("row: " + row.XPath);
+                foreach (HtmlNode cell in row.SelectNodes("th|td[@class='tddatos'][last()>position()]"))
                 {
-                    Console.WriteLine("row");
-                    foreach (HtmlNode cell in row.SelectNodes("th|td[@class='tddatos']"))
+                    Console.WriteLine("cell: " + cell.InnerText);
+                }
+                HtmlNode dataMaestro = row.SelectNodes("th|td[last()]//td[@class='tdprofesor']").First();
+
+
+
+                HtmlNode dataHorario = row.SelectNodes("th|td/table[@class='td1']").First();
+
+                if (dataHorario.InnerLength == 1)
+                    continue;
+
+                foreach (HtmlNode subHorario in dataHorario.SelectNodes("tbody/tr"))
+                {
+                    Console.WriteLine("cell: " + subHorario.XPath);
+
+                    foreach (HtmlNode cell in subHorario.SelectNodes("td"))
                     {
                         Console.WriteLine("cell: " + cell.InnerText);
                     }
                 }
+
+            }
             
         }
     }
