064ffbbe8d67ebe5721cdcdae7adfd258c440e42
Xinoji <jonathan.cn@live.com>
2023-06-02 23:37:50 -0600

    testing memory usage

--- Testing/Modelo.cs
+++ Testing/Modelo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,22 +10,37 @@ namespace Testing
     #region clases
     internal class Clase
     {
-        int NRC { get; set; }
-        int clave { get; set; }
-        string nombre { get; set; }
-        List<Horario> horarios { get; set; }
-        string maestro { get; set; }
-
-        Clase() 
+        public int NRC { get; set; }
+        public string Clave { get; set; }
+        public string Nombre { get; set; }
+        public List<Horario> Horarios { get; set; }
+        public string Maestro { get; set; }
+        public string Sec { get; set; }
+        public sbyte Creditos { get; set; }
+        public sbyte Cupos { get; set; }
+        public sbyte Disponibles { get; set; }
+        public Clase() 
         {
-            horarios = new List<Horario>();
+            Horarios = new List<Horario>();
         }
 
     }
-    internal class Horario 
-    { 
-        
-        
+    internal class Horario
+    {
+        public const int DIAS = 6;
+        public sbyte InitialHour { get; set; }
+        public sbyte FinalHour { get; set; }
+        public bool[] Days {get; set;}
+        public string Building { get; set; }
+        public string Classroom { get; set; }
+        //public DateOnly InicialDate { get; set; }
+        //public DateOnly FinalDate { get; set; }
+
+        public Horario() 
+        {
+            Days = new bool[6];
+        }
+
     }
     #endregion
 }
--- Testing/Program.cs
+++ Testing/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Text.Json;
 using System.Xml;
 using HtmlAgilityPack;
 
@@ -9,6 +11,10 @@ namespace Testing
     {
         static void Main(string[] args)
         {
+            
+            List<Clase> clases = new List<Clase>();
+            Clase clase;
+            Horario horario;
             var path = @"test.html";
 
             var doc = new HtmlDocument();
@@ -16,34 +22,60 @@ namespace Testing
             var tables = doc.DocumentNode.SelectNodes("//table[1]//tbody");
             var table = tables.First();
             Console.WriteLine("Found: " + table.XPath);
+
+
             foreach (HtmlNode row in table.SelectNodes("tr[position() > 2]"))
             {
-                Console.WriteLine("row: " + row.XPath);
-                foreach (HtmlNode cell in row.SelectNodes("th|td[@class='tddatos'][last()>position()]"))
-                {
-                    Console.WriteLine("cell: " + cell.InnerText);
-                }
-                HtmlNode dataMaestro = row.SelectNodes("th|td[last()]//td[@class='tdprofesor']").First();
+                clase = new Clase();
 
+                clase.NRC       =   int.Parse(  row.SelectSingleNode("th|td[@class='tddatos'][1]").InnerText);
+                clase.Clave     =               row.SelectSingleNode("th|td[@class='tddatos'][2]").InnerText;
+                clase.Nombre    =               row.SelectSingleNode("th|td[@class='tddatos'][3]").InnerText;
+                clase.Sec       =               row.SelectSingleNode("th|td[@class='tddatos'][4]").InnerText;
+                clase.Creditos  =   sbyte.Parse(row.SelectSingleNode("th|td[@class='tddatos'][5]").InnerText);
+                clase.Cupos     =   sbyte.Parse(row.SelectSingleNode("th|td[@class='tddatos'][6]").InnerText);
+                clase.Disponibles = sbyte.Parse(row.SelectSingleNode("th|td[@class='tddatos'][7]").InnerText);
 
+                HtmlNode dataHorario = row.SelectSingleNode("th|td/table[@class='td1']");
 
-                HtmlNode dataHorario = row.SelectNodes("th|td/table[@class='td1']").First();
-
-                if (dataHorario.InnerLength == 1)
-                    continue;
-
-                foreach (HtmlNode subHorario in dataHorario.SelectNodes("tbody/tr"))
-                {
-                    Console.WriteLine("cell: " + subHorario.XPath);
-
-                    foreach (HtmlNode cell in subHorario.SelectNodes("td"))
+                if (dataHorario.InnerLength != 1)
+                    foreach (HtmlNode subHorario in dataHorario.SelectNodes("tbody/tr"))
                     {
-                        Console.WriteLine("cell: " + cell.InnerText);
-                    }
-                }
+                        horario = new Horario();
+                        
+                        string Hora = subHorario.SelectSingleNode("td[2]").InnerText;
+                        
+                        horario.InitialHour = sbyte.Parse(Hora.Substring(0,2));
+                        
+                        horario.FinalHour = sbyte.Parse(Hora.Substring(5,2));
+                        string[] dias = subHorario.SelectSingleNode("td[3]").InnerText.Split(" ");
 
+                        for (int index = 0; index < Horario.DIAS; index++)
+                        {
+                            horario.Days[index] = dias[index] != ".";
+                        }
+
+                        horario.Building = subHorario.SelectSingleNode("td[4]").InnerText;
+                        horario.Classroom = subHorario.SelectSingleNode("td[5]").InnerText;
+
+                        clase.Horarios.Add(horario);
+                    }
+
+
+                HtmlNode dataMaestro = row.SelectSingleNode("th|td[last()]//td[@class='tdprofesor'][2]");
+                if (dataMaestro != null)
+                    clase.Maestro = dataMaestro.InnerText;
+
+                clases.Add(clase);
             }
-            
+            string jsonString = JsonSerializer.Serialize(clases);
+
+            if (File.Exists("test.json"))
+                File.Delete("test.json");
+
+            var file = File.AppendText("test.json");
+            file.WriteLine(jsonString);
+            file.Close();
         }
     }
 }
