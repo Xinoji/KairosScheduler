5ffb92e3d1be2ba83eba25f2b8ade8000c2072bd
Jonathan Isaac García Huerta <jonathan.cn@live.com>
2023-07-04 22:46:14 -0600

    Add WizzardContent Parent Class AND Text class

---
+++ Forms/Wizzard/WizardSelectType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Forms.Wizzard
+{
+    public partial class WizardSelectType : WizzardContent
+    {
+        public override string Title { get => "Seleccione el Horairo"; }
+        public override string Description { get => "Seleccione la forma en la que se elaborara el horario"; }
+        public WizardSelectType() : base()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Forms/Wizzard/WizzardContent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Forms.Wizzard
+{
+    public partial class WizzardContent : UserControl
+    {
+        public virtual string Title { get; set; }
+        public virtual string Description { get; set; }
+        protected WizzardContent()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
